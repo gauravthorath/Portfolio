6c8ca27408603031527fe7821e7441f6104baf18
Gaurav Thorat <gauravjobs25@gmail.com>
2024-09-12 00:50:13 +0200

    updated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,13 @@ const Home: React.FC = () => {
     <div className="p-4">
       <h1 className="text-2xl font-bold">Home</h1>
       <p>Welcome to my portfolio!</p>
+      <p>My name is Gaurav Thorat</p>
+      <p>I am a Frontend Developer</p>
+      <p>I am a learner</p>
+      <p>I am a programmer</p>
+      <p>I am a coder</p>
+      <p>I am a web developer</p>
+      <p>I am a web designer</p>
     </div>
   );
 };
