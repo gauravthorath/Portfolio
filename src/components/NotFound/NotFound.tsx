74eba28cedc9182494a50361ea2d70d968f12cea
Gaurav Thorat <gauravjobs25@gmail.com>
2024-09-24 07:10:29 +0200

    Updated not found component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -6,7 +6,8 @@ const NotFound: React.FC = () => {
       <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
       <p>Oops! The page you're looking for doesn't exist.</p>
       <p>
-        If you are not redirected, <a href="/Portfolio/">click here</a>.
+        If you are not redirected automatically,{" "}
+        <a href="/Portfolio/">click here</a>.
       </p>
     </div>
   );
