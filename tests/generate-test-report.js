0813ebd7e5f7193eba9d5892819e91faf00668d6
Gaurav Thorat <gauravjobs25@gmail.com>
2024-11-28 22:56:12 +0100

    "Updated import statements and added quotes around string literals in generate-test-report.js"

--- tests/generate-test-report.js
+++ tests/generate-test-report.js
@@ -1,18 +1,19 @@
-import { execSync } from 'child_process';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { execSync } from "node:child_process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Input JSON file path
-const jsonReportPath = path.resolve(__dirname, 'test-results/results.json');
+const jsonReportPath = path.resolve(__dirname, "test-results/results.json");
 
 // Command to generate CTRf report
 try {
-  execSync(`npx @ctrf/github-test-reporter summary ${jsonReportPath}`, {
-    stdio: 'inherit', // Ensures the output is shown in the Actions logs
-  });
-} catch (error) { // Explicitly typing error to 'any'
-  console.error('Failed to generate test report:', error.message);
-  process.exit(1); // Exit with failure
+	execSync(`npx @ctrf/github-test-reporter summary ${jsonReportPath}`, {
+		stdio: "inherit", // Ensures the output is shown in the Actions logs
+	});
+} catch (error) {
+	// Explicitly typing error to 'any'
+	console.error("Failed to generate test report:", error.message);
+	process.exit(1); // Exit with failure
 }
